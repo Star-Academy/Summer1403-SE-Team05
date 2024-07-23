2006ea034a43eec3ef8dfda5379fca8e9bd6db74
amiralirahimii <amiralirahimi82@gmail.com>
2024-07-23 18:18:07 +0330

    refactor: some cleaning

--- Phase02/Phase02-FullTextSearch/Services/InvertedIndex.cs
+++ Phase02/Phase02-FullTextSearch/Services/InvertedIndex.cs
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 ﻿using System.IO.Enumeration;
 
+=======
+﻿using Phase02_FullTextSearch.Utilities;
+>>>>>>> 0266b29 (refactor: some cleaning)
 namespace Phase02_FullTextSearch.Services;
 
 internal class InvertedIndex
@@ -13,7 +17,7 @@ internal class InvertedIndex
     private string[] TokenizeDocument(string document)
     {
         return document.Split(
-            new char[] { ' ', '\t', '\r', '\n', ',', '.', ';', ':', '!', '?', '-', '(', ')', '[', ']', '{', '}', '\"', '\'' },
+            Delimiters.Characters,
             StringSplitOptions.RemoveEmptyEntries
             );
     }
@@ -32,13 +36,17 @@ internal class InvertedIndex
     public void FillInvertedIndex(string documentFilesPath)
     {
         var documents = _fileReader.ReadAllFiles(documentFilesPath);
-        var captializedDocuments = _fileReader.CapitalizeDocumentsContent(documents);
-        foreach (var document in captializedDocuments)
+        var capitalizedDocuments = _fileReader.CapitalizeDocumentsContent(documents);
+        foreach (var document in capitalizedDocuments)
         {
             AddDocumentToInvertedIndex(document.Key, document.Value);
         }
     }
+<<<<<<< HEAD
     public IEnumerable<string> FindDocumentsContainingTagetWord(string targetWord)
+=======
+    private IEnumerable<string> FindDocumentsContainingTargetWord(string targetWord)
+>>>>>>> 0266b29 (refactor: some cleaning)
     {
         var upperTargetWord = targetWord.ToUpper();
         if (_invertedIndex.TryGetValue(upperTargetWord, out var resultDocumentNames))
@@ -46,4 +54,40 @@ internal class InvertedIndex
         else
             return Enumerable.Empty<string>();
     }
+<<<<<<< HEAD
+=======
+    private IEnumerable<string> FindMustWordsDocuments(List<string> words)
+    {
+        if (words.Count == 0)
+            return _invertedIndex.Values.SelectMany(v => v);
+        var allDocuments = _allDocumentsName.AsEnumerable();
+        return words.Aggregate(
+            allDocuments,
+            (current, word) => current.Intersect(FindDocumentsContainingTargetWord(word))
+            );
+    }
+    private IEnumerable<string> FindNoWordsDocuments(List<string> words)
+    {
+        if (words.Count == 0)
+            return _invertedIndex.Values.SelectMany(v => v);
+        var allDocuments = _allDocumentsName.AsEnumerable();
+        return words.Aggregate(
+            allDocuments,
+            (current, word) => current.Intersect(allDocuments.Except(FindDocumentsContainingTargetWord(word)))
+            );
+    }
+    private IEnumerable<string> FindAtLeast1WordsDocument(List<string> words)
+    {
+        if(words.Count == 0)
+            return _invertedIndex.Values.SelectMany(v => v);
+        var allDocuments = _allDocumentsName.AsEnumerable();
+        return allDocuments.Except(FindNoWordsDocuments(words));
+    }
+    public IEnumerable<string> FindDocumentsByCriteria(List<string> mustWords, List<string> atLeast1Word, List<string> noWords)
+    {
+        return FindMustWordsDocuments(mustWords)
+            .Intersect(FindAtLeast1WordsDocument(atLeast1Word))
+            .Intersect(FindNoWordsDocuments(noWords));
+    }
+>>>>>>> 0266b29 (refactor: some cleaning)
 }
--- Phase02/Phase02-FullTextSearch/UI/UserInterface.cs
+++ Phase02/Phase02-FullTextSearch/UI/UserInterface.cs
@@ -12,9 +12,39 @@ internal class UserInterface
 
     private void AskForTargetWordAndShowResult()
     {
+<<<<<<< HEAD
         Console.WriteLine("Enter the word you want to search for:");
         var targetWord = Console.ReadLine();
         var resultFileNames = _invertedIndex.FindDocumentsContainingTagetWord(targetWord);
+=======
+        List<string> andWords = new List<string>();
+        List<string> orWords = new List<string>();
+        List<string> notWords = new List<string>();
+
+        var words = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var word in words)
+        {
+            if (word.StartsWith('+'))
+                orWords.Add(word.Substring(1));
+            else if (word.StartsWith('-'))
+                notWords.Add(word.Substring(1));
+            else
+                andWords.Add(word);
+        }
+
+        return (andWords, orWords, notWords);
+    }
+    private bool AskCriteriaFromUser()
+    {
+        Console.WriteLine("Enter criteria: (No prefix for AND words, + prefix for OR words, - prefix for NOT words, exit! for exit");
+        var command = Console.ReadLine();
+        if (command.Equals("exit!"))
+            return false;
+
+        (List<string> andWords, List<string> orWords, List<string> notWords) = ParseCommand(command);
+        var resultFileNames = _invertedIndex.FindDocumentsByCriteria(andWords, orWords, notWords).ToList();
+>>>>>>> 0266b29 (refactor: some cleaning)
         Console.WriteLine("\nSearch Results:");
         Console.WriteLine("---------------");
 
@@ -27,11 +57,16 @@ internal class UserInterface
             Console.WriteLine("No files found containing the word.");
 
         Console.WriteLine("---------------\n");
+        return true;
     }
 
-    public void StartInteractingWithUser()
+    public void RunAskCriteriaFromUserLoop()
     {
+<<<<<<< HEAD
         while (true)
             AskForTargetWordAndShowResult();
+=======
+        while (AskCriteriaFromUser()) ;
+>>>>>>> 0266b29 (refactor: some cleaning)
     }
 }
